4445ed21bc26c4d320a08a74ac98077c3f57d94f
Arsalan-Adil <56950178+Mohammad-Arsalan-Adil@users.noreply.github.com>
2023-06-05 15:34:51 +0530

    Update streamlit_app.py
    added text files for menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('my Parents new healthy Diner')
 streamlit.header('breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale,Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
